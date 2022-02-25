e4098b9d1a4c2a7ece5df8fedc66c133cc48be10
ygofarias <ygo.f@hotmail.com>
2022-02-25 16:53:07 -0300

    Developed person create operation

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,8 +1,11 @@
 package one.digitalinnovation.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 //controlador que será acessado através de um api Rest
 @RestController
@@ -11,10 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
+    private PersonService personService;
+
+    //injeção de dependencia
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
     //operação http do verbo get
-    @GetMapping
-    public String getBook(){
-        return "API Test!";
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
     }
 
 }
